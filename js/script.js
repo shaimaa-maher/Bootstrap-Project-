63d5a547ebc3089ac266facb984d91ba08ce12f1
shaimaa-maher <shaimaa.maher1996@gmail.com>
2020-01-01 12:33:47 +0200

    Bootstrap lab2 project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function()
+  {
+  $(window).scroll(function() { // check if scroll event happened
+        
+    if ($(document).scrollTop() > 500) { // check if user scrolled more than 50 from top of the browser window
+      $(".navbar-fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+    } 
+    else
+     {
+      $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+     }
+  });
+});
